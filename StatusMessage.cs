717d4155d4c74a9e15ba8ca1efd891619830857d
ScoTTishCyclopSS <fed.timushev@gmail.com>
2021-03-15 15:52:37 +0300

    Ver 1.0
    + crc class
    + receiver class
    + status message class

---
+++ StatusMessage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace udp_random
+{
+	class StatusMessage
+	{
+		public static void SendBack(string ip, int port, string msg)
+		{
+			UdpClient sender = new UdpClient();
+			try
+			{
+				byte[] message = Encoding.ASCII.GetBytes(msg);
+				sender.Send(message, message.Length, ip, port);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				sender.Close();
+			}
+		}
+	}
+}
