8db29812c5e034b2bac75b0ea191eb85bc9b0ab1
R4ND0M-1D107 <57500131+R4ND0M-1D107@users.noreply.github.com>
2021-03-15 22:29:59 +0100

    1.3

--- Sender.cs
+++ Sender.cs
@@ -3,11 +3,10 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.IO;
-using System.Threading;
 using DamienG.Security.Cryptography;
+using System.Security.Cryptography;
 using System.Diagnostics;
-using System.Collections.Generic;
-
+using System.Threading;
 
 namespace UdpClientApp
 {
@@ -16,21 +15,19 @@ namespace UdpClientApp
         const int localPort = 8002;
         const int remotePort = 8001;
         const string remoteAddress = "25.84.62.46";
-        static string fileName;
-        static string path;
-        static bool running;
 
         const string RECEIVE_CONFIRM = "m1";
         const string RECEIVE_NULL = "m2";
         const int MAX_PACKET_SIZE = 1024;
         const int IDENTIFIER_SIZE = 1;
-        const int CRC_SIZE = 10;
+        const int CRC_SIZE = 4;
 
         const string NICKNAME =  "Meister351";
 
         static Sender program;
+        static int Timeout;
 
-        //C:/Users/overlord/Documents/Unity/
+        //C:/Users/overlord/Documents/Unity/Gunmancer/Gunmancer/Assets/Audio/
 
         public void Start()
         {
@@ -38,7 +35,7 @@ namespace UdpClientApp
                 Console.WriteLine("Enter '1' to send message");
                 Console.WriteLine("Enter '2' to send file");
                 Console.WriteLine("Enter '0' to exit");
-                program.SendOnlineStatus();
+                SendOnlineStatus();
         }
 
         public void Update()
@@ -51,13 +48,11 @@ namespace UdpClientApp
                 {
                     case 1:
                         number = -1;
-                        program.SendMessage();
-                        Console.WriteLine("Command done. Enter new command");
+                        SendMessage();
                         break;
                     case 2:
                         number = -1;
-                        program.SendFile();
-                        Console.WriteLine("Command done. Enter new command");
+                        SendFile();
                         break;
                     case 0:
                         MainClass.running = false;
@@ -66,106 +61,15 @@ namespace UdpClientApp
                         break;
                 }
         }
-
-        public byte[] encodePacket(char identifier, string packet)
-        {
-            byte[] messagePart = Encoding.ASCII.GetBytes(packet);
-            UInt32 crc = Crc32.Compute(messagePart);
-            byte[] crcBytes = BitConverter.GetBytes(crc);
-            byte[] firstPart = Encoding.ASCII.GetBytes('[');
-            byte[] secondPart = Encoding.ASCII.GetBytes(']' + identifier + packet);
-            byte[] message = Combine(firstPart, crcBytes, secondPart);
-            return message;
-        }
-
-        public static byte[] Combine(byte[] first, byte[] second, byte[] third)
-        {
-            byte[] ret = new byte[first.Length + second.Length + third.Length];
-            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
-            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
-            Buffer.BlockCopy(third, 0, ret, first.Length + second.Length,
-                             third.Length);
-            return ret;
-        }
-
-
-        public byte[] encodePacket(char identifier, byte[] packet)
-        {
-            UInt32 crc = Crc32.Compute(packet);
-            byte[] message = Encoding.ASCII.GetBytes(crc.ToString() + identifier + packet);
-            return message;
-        }
-        
-        public byte[] encodePacketOld(char identifier, string packet)
-        {
-            byte[] message = Encoding.ASCII.GetBytes(identifier + packet);
-            return message;
-        }
-
-        bool trySendPacket( UdpClient sender, byte[] packet)
-        {
-            //setup confiramtion receiver
-            IPEndPoint remoteIp = new IPEndPoint(IPAddress.Parse(remoteAddress), localPort);
-            byte[] data = Encoding.ASCII.GetBytes(RECEIVE_NULL);
-            UdpClient receiver = new UdpClient(localPort);
-
-            //send packet
-            sender.Send(packet, packet.Length, remoteAddress, remotePort);
-
-            //start timer
-            var waitTime = new TimeSpan(0, 0, 1);
-            var waitUntil = DateTime.Now + waitTime;
-
-            //wait for confiramation
-            while (DateTime.Now <= waitUntil)
-            {
-                data = receiver.Receive(ref remoteIp);
-                if (System.Text.Encoding.UTF8.GetString(data) != RECEIVE_NULL) //if received something
-                {
-                    receiver.Close();
-                    if (System.Text.Encoding.UTF8.GetString(data) == RECEIVE_CONFIRM) // if received confirmation
-                    {
-                        Console.WriteLine("Confirm");
-                        return true;
-                    } else
-                    {
-                        Console.WriteLine("!Confirm");
-                        return false;
-                    }
-                }
-            }
-
-            receiver.Close();
-            Console.WriteLine("TimeOut");
-            return false;
-        }
-
-        public void SendOnlineStatus()
-        {
-            UdpClient sender = new UdpClient();
-            try
-            {
-                byte[] packet = encodePacketOld('s', NICKNAME);
-                sender.Send(packet, packet.Length, remoteAddress, remotePort);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                sender.Close();
-            }
-        }
-
-        public void SendMessage()
+ 
+        void SendMessage()
         {
             Console.WriteLine("Enter message : ");
             string messageStr = Console.ReadLine();
             UdpClient sender = new UdpClient();
             try
             {
-                byte[] packet = encodePacketOld('m', messageStr);
+                byte[] packet = encodePacket('m', messageStr);
                 bool receivedCorrectly = false;
                 while (!receivedCorrectly)
                 {
@@ -181,20 +85,20 @@ namespace UdpClientApp
                 sender.Close();
             }
         }
-
-        public void SendFile()
+ 
+        void SendFile(string path, string fileName)
         {
-            Console.WriteLine("Enter file path : ");
-            path = Console.ReadLine();
-            Console.WriteLine("Enter file name : ");
-            fileName = Console.ReadLine();
-
             UdpClient sender = new UdpClient();
 
+            Timeout = 0;
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var md5 = MD5.Create();
 
             try
             {
-                byte[] packet =  encodePacket('f', fileName);
+                long fileSize = new System.IO.FileInfo(path + fileName).Length;
+                byte[] packet = encodePacket('f', "[" + fileSize + "]" + fileName);
 
                 //send file name
                 bool receivedCorrectly = false;
@@ -205,18 +109,41 @@ namespace UdpClientApp
 
                 //open file
                 FileStream file = File.OpenRead(path + fileName);
+                byte[] fileData = new byte[file.Length];
+                file.Read(fileData, 0, (int)file.Length);
+                byte[] hash = md5.ComputeHash(fileData);
+
                 byte[] buffer = new byte[MAX_PACKET_SIZE - CRC_SIZE - IDENTIFIER_SIZE];
 
+                file.Position = 0;
+                long sentSize = 0;
                 //send file
                 while (file.Read(buffer, 0, buffer.Length) > 0)
                 {
                     packet = encodePacket('p', buffer);
 
+                    receivedCorrectly = false;
                     while (!receivedCorrectly)
                     {
                         receivedCorrectly = trySendPacket(sender, packet);
                     }
+
+                    sentSize += 1019;
+                    float percent = ((float)sentSize / (float)fileSize) * 100;
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                    if (percent >= 100)
+                    {
+                        percent = 100.00f;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    Console.Write("\r{0:000.00}%   ", percent);
                 }
+                
+                Console.WriteLine();
+                Console.ResetColor();
 
                 file.Close();
 
@@ -228,6 +155,161 @@ namespace UdpClientApp
                 {
                     receivedCorrectly = trySendPacket(sender, packet);
                 }
+
+                if(!fileCorrect(sender))
+                {
+                    SendFile(path, fileName);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Corrupted!! Sending again.");
+                    Console.ResetColor();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                sender.Close();
+                sw.Stop();
+                Console.WriteLine("Timeout : " + Timeout);
+                Console.WriteLine("Elapsed={0}", sw.Elapsed);
+            }
+        }
+
+        bool fileCorrect(UdpClient sender)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(RECEIVE_NULL);
+            while(System.Text.Encoding.UTF8.GetString(data) == RECEIVE_NULL)
+            {
+                IPEndPoint remoteIp = new IPEndPoint(IPAddress.Parse(remoteAddress), localPort);
+                UdpClient receiver = new UdpClient(localPort);
+                receiver.Client.ReceiveTimeout = 200;
+
+                try
+                {
+                    data = receiver.Receive(ref remoteIp);
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine(e.Message);
+                    Timeout++;
+                }
+
+                if (System.Text.Encoding.UTF8.GetString(data) != RECEIVE_NULL) //if received something
+                {
+                    receiver.Close();
+                    if (System.Text.Encoding.UTF8.GetString(data) == "h1") // if received confirmation
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+                receiver.Close();
+
+                byte[] packet = encodePacket('h', "HashNotDelivered");
+                bool receivedCorrectly = false;
+                while (!receivedCorrectly)
+                {
+                    receivedCorrectly = trySendPacket(sender, packet);
+                }
+            }
+
+            return false;
+        }
+
+        void SendFile()
+        {
+            Console.WriteLine("Enter file path : ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string path = Console.ReadLine();
+            Console.ResetColor();
+            Console.WriteLine("Enter file name : ");
+            Console.ForegroundColor = ConsoleColor.White;
+            string fileName = Console.ReadLine();
+            Console.ResetColor();
+
+            SendFile(path, fileName);
+        }
+
+        byte[] encodePacket(char identifier, string packet)
+        {
+            byte[] messagePart = Encoding.ASCII.GetBytes(packet);
+            UInt32 crc = Crc32.Compute(messagePart);
+            byte[] crcBytes = BitConverter.GetBytes(crc);
+            byte[] message = Encoding.ASCII.GetBytes(identifier + packet);
+            message = Combine(crcBytes, message);
+            return message;
+        }
+
+        byte[] encodePacket(char identifier, byte[] packet)
+        {
+            UInt32 crc = Crc32.Compute(packet);
+            byte[] crcBytes = BitConverter.GetBytes(crc);
+            byte[] message = addByteToArray(crcBytes, Convert.ToByte(identifier));
+            message = Combine(message, packet);
+            return message;
+        }
+        
+        byte[] encodePacketOld(char identifier, string packet)
+        {
+            byte[] message = Encoding.ASCII.GetBytes(identifier + packet);
+            return message;
+        }
+
+        bool trySendPacket( UdpClient sender, byte[] packet)
+        {
+            //setup confiramtion receiver
+            IPEndPoint remoteIp = new IPEndPoint(IPAddress.Parse(remoteAddress), localPort);
+            byte[] data = Encoding.ASCII.GetBytes(RECEIVE_NULL);
+            UdpClient receiver = new UdpClient(localPort);
+            receiver.Client.ReceiveTimeout = 90;
+
+
+            //send packet
+            sender.Send(packet, packet.Length, remoteAddress, remotePort);
+
+
+                try
+                {
+                    data = receiver.Receive(ref remoteIp);
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine(e.Message);
+                    Timeout++;
+                }
+
+                if (System.Text.Encoding.UTF8.GetString(data) != RECEIVE_NULL) //if received something
+                {
+                    receiver.Close();
+                    if (System.Text.Encoding.UTF8.GetString(data) == RECEIVE_CONFIRM) // if received confirmation
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+
+            receiver.Close();
+            return false;
+        }
+
+        void SendOnlineStatus()
+        {
+            UdpClient sender = new UdpClient();
+            try
+            {
+                byte[] packet = encodePacket('s', NICKNAME);
+
+                sender.Send(packet, packet.Length, remoteAddress, remotePort);
             }
             catch (Exception ex)
             {
@@ -237,6 +319,22 @@ namespace UdpClientApp
             {
                 sender.Close();
             }
+        }
+        
+        public static byte[] Combine(byte[] first, byte[] second)
+        {
+            byte[] ret = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+            return ret;
+        }
+  
+        public byte[] addByteToArray(byte[] bArray, byte newByte)
+        {
+            byte[] newArray = new byte[bArray.Length + 1];
+            bArray.CopyTo(newArray, 0);
+            newArray[bArray.Length] = newByte;
+            return newArray;
         }
     }
 }
